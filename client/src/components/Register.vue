ed8858bc7fbace58d9608aab302cd26c56164253
khihadysucahyo <khi.hady@gamatechno.com>
2019-01-20 02:09:34 +0700

    register procces

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,42 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout align-center justify-center row fill-height>
+    <v-flex xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="red" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-
-  </div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            placeholder="email"
+          ></v-text-field>
+          <!-- <br> -->
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            type="password"
+            placeholder="password"
+          ></v-text-field>
+          <!-- <br> -->
+          <div class="message" v-html="message"></div>
+          <br>
+          <v-btn
+            class="red"
+            @click="validate">
+            Register
+          </v-btn>
+          </v-form>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -28,21 +46,44 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      message: null,
+      alert: true,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 8 || 'Min 8 characters',
+        v => v.length <= 32 || 'Max 32 characters'
+      ]
     }
   },
   methods: {
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.register()
+      }
+    },
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        this.message = 'Succesfully Registered'
+        console.log(response.data)
+      } catch (err) {
+        this.message = err.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.message {
+  color: green;
+}
 </style>
---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'tabtracker',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    options: {
+      dialect: process.env.DIALECT || 'mysql',
+      host: 'localhost',
+      storage: './tabtracker.mysql'
+    }
+  }
+}
